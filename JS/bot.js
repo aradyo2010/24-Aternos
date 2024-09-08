bfd7212aad6a4fd885cf8da6a8648800bd4aec1b
aradyo2010 <adrijbhattacharyya2@gmail.com>
2024-09-08 13:04:35 +0530

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,8 +4,8 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
+    host: "chessboy2010.aternos.me",
+    port: "50863",
     username: "24ATERNOSBOT",
     version: false
     })
